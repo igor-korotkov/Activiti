7a5408ff16214ed314065b6de5effa9a7512b1d3
gorbunkov <gorbunkov@haulmont.com>
2015-05-12 12:25:53 +0400

    Modeler form for process roles

---
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/cuba/cuba-properties.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var KISBPM = KISBPM || {};
+KISBPM.PROPERTY_CONFIG["oryx-procroles-multiplecomplex"] = {
+    "readModeTemplateUrl": "editor-app/configuration/properties/cuba/proc-roles-display-template.html",
+    "writeModeTemplateUrl": "editor-app/configuration/properties/cuba/proc-roles-write-template.html"
+}
