b025df0d8bcebab9446a8621cc4faedad2181851
gorbunkov <gorbunkov@haulmont.com>
2015-06-24 16:12:29 +0400

    install.bat modification

--- install-bpm.bat
+++ install-bpm.bat
@@ -1,8 +1,9 @@
 set TOMCAT_PATH=d:\projects\bpm-trunk\build\tomcat
-set MODELER_VERSION=5.17.0.cuba.1-SNAPSHOT
+set MODELER_VERSION=5.17.0.cuba.2-SNAPSHOT
 cd modules\activiti-webapp-explorer2
-call mvn assembly:single install:install-file deploy:deploy-file -P cuba
-7z.exe x -y -o%TOMCAT_PATH%\webapps\bpm target\cuba-modeler-%MODELER_VERSION%.zip 
+rem call mvn assembly:single install:install-file deploy:deploy-file -P cuba
+call mvn assembly:single install:install-file -P cuba --settings settings.xml
+7z.exe x -y -o%TOMCAT_PATH%\webapps\bpm target\cuba-modeler-%MODELER_VERSION%.zip
 cd ../..
 
 cd modules\activiti-json-converter
