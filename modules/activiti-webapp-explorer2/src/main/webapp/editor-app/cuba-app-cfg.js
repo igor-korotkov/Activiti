8cf874933ea50d3da4ab089f736d7ebbbcfff707
gorbunkov <gorbunkov@haulmont.com>
2015-07-08 16:38:14 +0400

    Modeler root URL automatic evaluation

--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/cuba-app-cfg.js
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/cuba-app-cfg.js
@@ -2,6 +2,11 @@
 
 var CUBA = CUBA || {};
 
+var getModelerRoot = function() {
+    var href = window.location.href;
+    return href.substring(window.location.origin.length, href.indexOf("/modeler/") + "/modeler/".length);
+}
+
 CUBA.CONFIG = {
-    modelerRoot: '/app/dispatch/modeler'
+    modelerRoot: getModelerRoot()
 }
--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/cuba-custom-app-cfg.js
+++
@@ -1,5 +0,0 @@
-/**
- * Replace this file by your own one in your project if you need to replace or add additional configuration
-*/
-
-//CUBA.CONFIG.modelerRoot = '/bpm/dispatch/modeler';
