060521b05080eecedb342514db2043ae04a50941
gorbunkov <gorbunkov@haulmont.com>
2015-07-10 15:05:00 +0400

    json-converter pom fixed

--- install-bpm.bat
+++ install-bpm.bat
@@ -1,5 +1,5 @@
-set TOMCAT_PATH=d:\projects\bpm-trunk\build\tomcat
-set MODELER_VERSION=5.17.0.cuba.5-SNAPSHOT
+set TOMCAT_PATH=d:\projects\bpm_5_6\build\tomcat
+set MODELER_VERSION=5.17.0.cuba.8
 cd modules\activiti-webapp-explorer2
 rem call mvn assembly:single install:install-file deploy:deploy-file -P cuba
 call mvn assembly:single install:install-file -P cuba --settings settings.xml  -Dcuba.modeler.version=%MODELER_VERSION%
@@ -7,5 +7,5 @@ call mvn assembly:single install:install-file -P cuba --settings settings.xml  -
 cd ../..
 
 cd modules\activiti-json-converter
-call mvn package install -Dmaven.test.skip=true -P distro -Dcuba.modeler.version=%MODELER_VERSION%
+call mvn package install:install-file -Dmaven.test.skip=true -P cuba -Dcuba.modeler.version=%MODELER_VERSION%
 cd ../..
