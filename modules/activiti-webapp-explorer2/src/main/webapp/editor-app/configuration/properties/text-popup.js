0639ac4756171e3f75380d87612b76333b888201
shkuratko <shkuratko@haulmont.com>
2017-07-04 22:17:51 +0400

    add output variables

--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/properties/text-popup.js
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/properties/text-popup.js
@@ -1,18 +1,21 @@
+var jsonString = angular.element(document.getElementById('textarea')).scope().getPropertyValue();
+if (jsonString) {
+  var jsonObject = JSON.parse(jsonString);
+}
+
+
 ace.require("ace/ext/language_tools");
 var editor = ace.edit("editor");
 editor.setOptions({
   enableBasicAutocompletion: true
 });
 editor.getSession().on('change', function () {
-  var value = editor.getSession().getValue();
-  document.getElementById("textarea").value = value;
+  changeJson();
   jQuery("textarea").change();
 });
 editor.getSession().setMode("ace/mode/groovy");
 
-var val = angular.element(document.getElementById('textarea')).scope().getPropertyValue();
-editor.setValue(val, 1);
-editor.focus();
+
 
 var templateSelect = jQuery('#templateSelect');
 
@@ -48,6 +51,17 @@ httpGetAsync(getScriptTemplateListControllerPath(), function (responseText) {
       sel.appendChild(opt);
     }
   }
+  if (jsonObject) {
+    editor.setValue(utility.unescapeQuotes(jsonObject.script), 1);
+    jsonObject.vars.forEach(function (item, i, arr) {
+      jQuery("#outTable").append('<tr><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.name + '"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.type + '"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.description + '"></td></tr>')
+    })
+    changeJson();
+    jQuery("#outTable tr").not(':first').click(function () {
+      jQuery(this).addClass('selected').siblings().removeClass('selected');
+    })
+    editor.focus();
+  }
 });
 
 function getScriptTemplateListControllerPath() {
@@ -74,3 +88,67 @@ function httpGetAsync(theUrl, callback) {
   xmlHttp.open("GET", theUrl, true);
   xmlHttp.send(null);
 }
+
+
+
+jQuery("#removeBtn").click(function () {
+  jQuery('.selected').remove();
+  changeJson();
+})
+
+jQuery("#addBtn").click(function () {
+  jQuery("#outTable").append('<tr><td><input onchange="textChanged();" oninput="this.onchange();" type="text"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text"></td></tr>')
+  jQuery("#outTable tr").unbind("click");
+  jQuery("#outTable tr").not(':first').click(function () {
+    jQuery(this).addClass('selected').siblings().removeClass('selected');
+  })
+})
+
+function textChanged() {
+  changeJson();
+}
+
+function changeJson() {
+  var script = editor.getSession().getValue();
+  var JSONString = "{" + "\"script\":\"" + utility.escapeQuotes(script) + "\", " +
+    "\"vars\":[" +
+    getOutVariablesTableJson() +
+    "]" + "}";
+  document.getElementById("textarea").value = JSONString;
+  jQuery("textarea").change();
+}
+
+function getOutVariablesTableJson() {
+  var result = "";
+  var total = jQuery('#outTable').find('tbody tr').not(':first').length;
+  jQuery('#outTable').find('tbody tr').not(':first').each(function (index) {
+    var name = jQuery(this).find("td:eq(0) input[type='text']").val();
+    var type = jQuery(this).find("td:eq(1) input[type='text']").val();
+    var descrpition = jQuery(this).find("td:eq(2) input[type='text']").val();
+    result = result + "{\"name\": \"" + utility.escapeQuotes(name) + "\", \"type\": \"" + utility.escapeQuotes(type) + "\", \"description\": \"" + utility.escapeQuotes(descrpition) + "\"}";
+    if (index !== total - 1) {
+      result = result + ',';
+    }
+
+  })
+  return result;
+}
+
+
+var utility = {
+  escapeQuotes: function (string) {
+    return string.replace(/"/g, '\\"');
+  },
+  unescapeQuotesToQuotChr: function (string) {
+    return string.replace(/"/g, '&quot;');
+  },
+  unescapeQuotes: function (string) {
+    return string.replace(/"/g, '\"');
+  }
+
+
+};
+
+jQuery("#outTable tr").not(':first').click(function () {
+  jQuery(this).addClass('selected').siblings().removeClass('selected');
+})
