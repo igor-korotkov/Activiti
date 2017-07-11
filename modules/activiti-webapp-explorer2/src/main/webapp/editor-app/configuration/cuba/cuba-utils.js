07b42609ec4e640184da678ad5ee5996bd4c01c1
shkuratko <shkuratko@haulmont.com>
2017-07-11 19:09:42 +0400

    TB SCORING 77

--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/cuba/cuba-utils.js
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/cuba/cuba-utils.js
@@ -1,3 +1,7 @@
+var childShapes;
+
+var angScope;
+
 CubaStencilUtils = {
     fillMainAndAdvancedProperties: function (selectedItem, stencil) {
         selectedItem.mainProperties = [];
@@ -21,126 +25,55 @@ CubaStencilUtils = {
         }
     },
     getAvailableVariablesForSelectedShape: function () {
-
-        getBoundedList()
-
-
-    }
-}
-var result = new Tree();
-
-var childShapes;
-
-function getBoundedList() {
-
-    var jsonModel = angular.element(document.getElementsByClassName('ng-scope')).scope().editor.getJSON();
-    childShapes = jsonModel.childShapes;
-
-    for (var i = 0; i < childShapes.length; i++) {
-        if (childShapes[i].stencil.id == 'StartNoneEvent') {
-            addTreeNode(childShapes[i])
-
-            console.log('found start none event node')
-        }
-    }
-    console.log(result)
-}
-
-function addTreeNode(data, toNodeData) {
-    result.add(data.resourceId, toNodeData)
-    for (var i = 0; i < data.outgoing.length; i++) {
-        addTreeNode(getShapeById(data.outgoing[i].resourceId), data.resourceId)
+        angScope = angular.element(document.getElementsByClassName('modal-dialog')).scope();
+        return getAffectedNodesForSelectedShape();
     }
 }
 
-function getShapeById(id) {
-
-    for (var i = 0; i < childShapes.length; i++) {
-        if (childShapes[i].resourceId == id) {
-            return childShapes[i];
-        }
-    }
-    return null;
-}
-
-function Node(data) {
-    this.data = data;
-    this.children = [];
-    this.parent = [];
-}
-
-function Tree() {
-    this.root = null;
-}
-
-Tree.prototype.add = function (data, toNodeData) {
-    var node = new Node(data);
-    var parent = toNodeData ? this.findBFS(toNodeData) : null;
-    if (parent) {
-        parent.children.push(node);
-        node.parent.push(parent);
-    } else {
-        if (!this.root) {
-            this.root = node;
-        } else {
-            return 'Root node is already assigned';
-        }
-    }
-};
-
-Tree.prototype.findBFS = function (data) {
-    var queue = [this.root];
+function getAffectedNodesForSelectedShape() {
+    var result = [];
+    var selectedShapeId = angScope.selectedShape.resourceId
+    var jsonModel = angScope.editor.getJSON();
+    childShapes = jsonModel.childShapes
+    var queue = [selectedShapeId];
     while (queue.length) {
-        var node = queue.shift();
-        if (node.data === data) {
-            return node;
-        }
-        for (var i = 0; i < node.children.length; i++) {
-            queue.push(node.children[i]);
-        }
-    }
-    return null;
-};
-
-// var currentShape = childShapes[i];
-//      var shape = {}
-//      shape.id = currentShape.resourceId
-//      shape.in = [];
-//      shape.out = [];
-//      for (var y = 0; y < currentShape.outgoing.length; y++) {
-//          shape.out.push(currentShape.outgoing[y].resourceId)
-//      }
-//      for (var y = 0; y < childShapes.length; y++) {
-//          for (z = 0; z < childShapes[y].outgoing.length; z++) {
-//              if (childShapes[y].outgoing[z].resourceId == shape.id) {
-//                  shape.in.push(childShapes[y].resourceId);
-//              }
-//          }
-//      }
-//      result.push(shape)
-
-
-function createShape(currentShape, callFromShape) {
-
-    for (var i = 0; i < result.length; i++) {
-        if (result[i].id == currentShape.resourceId && callFromShape) {
-            var shape = result[i];
-            shape.in.push(callFromShape.id);
-            return shape;
+        var nodeId = queue.shift()
+        for (var i = 0; i < childShapes.length; i++) {
+            var currentShape = childShapes[i];
+            var outgoingShapes = currentShape.outgoing
+            for (var y = 0; y < outgoingShapes.length; y++) {
+                if (outgoingShapes[y].resourceId == nodeId) {
+                    if (!result.includes(currentShape.resourceId)) {
+                        var nodeVariblesWrapper = {}
+                        nodeVariblesWrapper.id = currentShape.resourceId
+                        nodeVariblesWrapper.vars = getVariables(currentShape)
+                        result.push(nodeVariblesWrapper)
+                    }
+                    queue.push(currentShape.resourceId)
+                }
+            }
         }
     }
-    var shape = {};
-    shape.id = currentShape.resourceId;
-    shape.in = [];
-    if (callFromShape) {
-        shape.in.push(callFromShape.id);
+    return result;
+}
+
+function getVariables(shape) {
+    var vars = [];
+    var type = shape.stencil.id
+    switch (type) {
+        case 'ScriptTask':
+            var text = shape.properties.scripttext;
+            var scriptJson = JSON.parse(text);
+            vars = scriptJson.vars;
+            break;
+        case 'IntegrationNode':
+            var text = shape.properties.beanselect;
+            var beanJson = JSON.parse(text);
+            var variable = {}
+            variable.name = beanJson.outputName
+            variable.type = beanJson.outputType
+            vars.push(variable)
+            break;
     }
-    shape.out = [];
-    var outgoingShapes = currentShape.outgoing;
-    if (outgoingShapes) {
-        for (var i = 0; i < outgoingShapes.length; i++) {
-            shape.out.push(createShape(outgoingShapes[i], shape));
-        }
-    }
-    return shape;
+    return vars;
 }
--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/properties/text-popup.js
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/properties/text-popup.js
@@ -9,29 +9,59 @@ ace.require("ace/ext/language_tools");
 var editor = ace.edit("editor");
 
 
+var nodes = CubaStencilUtils.getAvailableVariablesForSelectedShape()
+var inputParams = angular.element(document.getElementById('textarea')).scope().inputParameters;
+var worlListForAutoComplete = [];
+fillWordList();
 
+function fillWordList() {
+  for (var i = 0; i < inputParams.length; i++) {
+    worlListForAutoComplete.push(inputParams[i].name);
+  }
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i]
+    var vars = node.vars
+    for (var y = 0; y < vars.length; y++) {
+      worlListForAutoComplete.push(nodes[i].vars[y].name);
+    }
+  }
+}
+
+fillInTable();
+
+function fillInTable() {
+  //nodes = node id and variables from prev nodes
+  //input params = process input params
+
+  if (!nodes.length && !inputParams.length) {
+    jQuery("#inputVariablesWrapper").hide();
+  } else {
+    jQuery("#inputVariablesWrapper").show();
+  }
+  for (var i = 0; i < inputParams.length; i++) {
+    jQuery("#inTable").append('<tr><td>' + inputParams[i].name + '</td><td>' + inputParams[i].parameterType + '</td><td>' + inputParams[i].constant + '</td><td>' + inputParams[i].valueStr + '</td></tr>')
+  }
+
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i]
+    var vars = node.vars
+    for (var y = 0; y < vars.length; y++) {
+      jQuery("#inTable").append('<tr><td>' + nodes[i].vars[y].name + '</td><td>' + nodes[i].vars[y].type + '</td><td>' + '</td><td>' + '</td></tr>')
+    }
+  }
+}
 
 var variablesWordCompleter = {
-  getCompletions : function (editor, session, pos, prefix, callback) {
-    var wordList = [];
-    var inputParams = angular.element(document.getElementById('textarea')).scope().inputParameters;
-    for (var i = 0; i < inputParams.length; i++) {
-      wordList.push(inputParams[i].name);
-    }
-    callback(null, wordList.map(function (word) {
+  getCompletions: function (editor, session, pos, prefix, callback) {
+    callback(null, worlListForAutoComplete.map(function (word) {
       return {
         caption: word,
         value: word,
         meta: "static"
       };
     }));
-
   }
 }
-
-
-CubaStencilUtils.getAvailableVariablesForSelectedShape()
-
 
 var langTools = ace.require("ace/ext/language_tools");
 editor.getSession().on('change', function () {
@@ -56,13 +86,11 @@ templateSelect.change(function () {
       editor.focus();
     });
   }
-
 })
 
 httpGetAsync(getScriptTemplateListControllerPath(), function (responseText) {
   var names = JSON.parse(responseText);
   var sel = document.getElementById('templateSelect');
-
   var opt = document.createElement('option');
   opt.innerHTML = '';
   opt.value = '';
@@ -84,7 +112,6 @@ httpGetAsync(getScriptTemplateListControllerPath(), function (responseText) {
     for (var i = 0; i < scriptRows.length; i++) {
       scriptValue = scriptValue + scriptRows[i] + '\n';
     }
-    123
     editor.setValue(utility.unescapeQuotes(scriptValue), 1);
     jsonObject.vars.forEach(function (item, i, arr) {
       jQuery("#outTable").append('<tr><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.name + '"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.type + '"></td><td><input onchange="textChanged();" oninput="this.onchange();" type="text" value = "' + item.description + '"></td></tr>')
