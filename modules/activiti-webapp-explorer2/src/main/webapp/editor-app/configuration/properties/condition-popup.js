3fc5a3688f12a922dfaf82564e4ae80cce1710d3
SergeyShkuratko <shkuratko@haulmont.com>
2017-08-25 14:22:49 +0400

    'TB_SCORING-127'

---
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/configuration/properties/condition-popup.js
@@ -0,0 +1,140 @@
+var jsonString = angular.element(document.getElementById('textarea')).scope().getPropertyValue();
+
+var typeList = ["Integer", "Double", "String", "Boolean", "Money", "Date", "Time", "DateTime", "Map", "Set", "List"]
+
+ace.require("ace/ext/language_tools");
+var editor = ace.edit("editor");
+
+
+var nodes = CubaStencilUtils.getAvailableVariablesForSelectedShape()
+var inputParams = angular.element(document.getElementById('textarea')).scope().inputParameters;
+var worlListForAutoComplete = [];
+fillWordList();
+
+function fillWordList() {
+  for (var i = 0; i < inputParams.length; i++) {
+    worlListForAutoComplete.push(inputParams[i].name);
+  }
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i]
+    var vars = node.vars
+    for (var y = 0; y < vars.length; y++) {
+      worlListForAutoComplete.push(nodes[i].vars[y].name);
+    }
+  }
+}
+
+var variablesWordCompleter = {
+  getCompletions: function (editor, session, pos, prefix, callback) {
+    callback(null, worlListForAutoComplete.map(function (word) {
+      return {
+        caption: word,
+        value: word,
+        meta: "static"
+      };
+    }));
+  }
+}
+
+var langTools = ace.require("ace/ext/language_tools");
+editor.getSession().on('change', function () {
+  changeJson();
+  jQuery("textarea").change();
+});
+editor.getSession().setMode("ace/mode/groovy");
+
+editor.setOptions({
+  enableBasicAutocompletion: true
+});
+editor.completers.push(variablesWordCompleter);
+
+
+function textChanged() {
+  changeJson();
+}
+
+function changeJson() {
+  var script = editor.getSession().getValue();
+
+  var scriptLines = script.split('\n');
+  var scriptLinesString = '';
+  for (var i = 0; i < scriptLines.length; i++) {
+    var line = utility.escapeQuotes(scriptLines[i]);
+    scriptLinesString = scriptLinesString +  line ;
+    if (i + 1 < scriptLines.length) {
+      scriptLinesString = scriptLinesString + ',';
+    }
+  }
+  var JSONString =  scriptLinesString;
+  document.getElementById("textarea").value = JSONString;
+  jQuery("textarea").change();
+}
+
+var utility = {
+  escapeQuotes: function (string) {
+    return string.replace(/"/g, '\\"');
+  },
+  unescapeQuotesToQuotChr: function (string) {
+    return string.replace(/"/g, '&quot;');
+  },
+  unescapeQuotes: function (string) {
+    return string.replace(/"/g, '\"');
+  }
+};
+
+
+var comboplets = []
+
+function initAutoComplete() {
+  var input = document.getElementsByClassName("inType");
+  for (var i = 0; i < input.length; i++) {
+    if (!input[i].parentElement.classList.contains('awesomplete')) {
+      var comboplete = new Awesomplete(input[i], {
+        minChars: 1,
+        list: ["Integer", "Double", "String", "Boolean", "Money", "Date", "Time", "DateTime", "Map", "Set", "List"]
+      });
+      var dropdownBtn = input[i].parentElement.parentElement.getElementsByClassName('dropdown-btn')[0];
+      var obj = {}
+      obj.c = comboplete
+      obj.b = dropdownBtn
+      comboplets.push(obj);
+      initDropDownListeners();
+    }
+  }
+}
+
+function initDropDownListeners() {
+  comboplets.each(function (item, i, arr) {
+    if (!item.added) {
+      item.added = true;
+      item.b.addEventListener("click", function () {
+        closeAllOtherComboplets(item.c);
+        var comboplete = item.c;
+        if (comboplete.ul.childNodes.length === 0) {
+          comboplete.minChars = 0;
+          comboplete.evaluate();
+        } else if (comboplete.ul.hasAttribute('hidden')) {
+          comboplete.open();
+        } else {
+          comboplete.close();
+        }
+      })
+    }
+  })
+}
+
+function closeAllOtherComboplets(item) {
+  comboplets.each(function (obj, i, arr) {
+    if (!obj.c.ul.hasAttribute('hidden') && obj.c !== item) {
+      obj.c.close();
+    }
+  })
+}
+ if (jsonString) {
+console.log(jsonString)
+    editor.setValue(utility.unescapeQuotes(jsonString), 1);
+	editor.focus();
+ }
+if (!jsonString) {
+  changeJson();
+}
