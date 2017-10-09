57b6ee89f8647569e8186b0140048af8ea3976fc
Valentin Malyshev <malyshev@haulmont.com>
2017-10-09 18:23:38 +0400

    Disabled alerts

--- modules/activiti-webapp-explorer2/src/main/webapp/editor-app/editor/oryx.debug.js
+++ modules/activiti-webapp-explorer2/src/main/webapp/editor-app/editor/oryx.debug.js
@@ -18463,11 +18463,11 @@ ORYX.Plugins.OpenEdit = Clazz.extend({
             func = KisBpmBeanSelectCtrl[2];
             sc = this.getScope("beanselect", shape)
 		} else if (shape.properties['oryx-scripttext'] !== undefined) {
-            alert("invoke ctrl oryx-scripttext");
+            // alert("invoke ctrl oryx-scripttext");
             func = KisBpmTextPropertyCtrl[2];
             sc = this.getScope("scripttext", shape)
         } else if (shape.properties['oryx-conditionsequenceflow'] !== undefined) {
-            alert("invoke ctrl oryx-conditionsequenceflow");
+            // alert("invoke ctrl oryx-conditionsequenceflow");
             func = KisBpmConditionFlowSelectCtrl[2];
             sc = this.getScope("conditionsequenceflow", shape)
 		} else {
