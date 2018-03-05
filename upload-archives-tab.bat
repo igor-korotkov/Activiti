970a23548a68048db69a39a1f233574342c974e1
korotkov <korotkov@haulmont.com>
2018-03-05 22:36:44 +0400

    Upload tasks fixed, version updated to .11 release

--- upload-archives-tab.bat
+++ upload-archives-tab.bat
@@ -1,14 +1,15 @@
-set MODELER_VERSION=5.20.bespoke.7-SNAPSHOT
+set MODELER_VERSION=5.20.bespoke.11
+set UPLOAD_REPO_PATH = http://10.1.20.55:8082/nexus/content/groups/cuba-group/repositories/releases
 
 cd modules\activiti-webapp-explorer2
-rem call mvn assembly:single -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
-call mvn deploy:deploy-file -Dcuba.repository.url=http://10.1.20.55:8082/nexus/content/groups/cuba-group/repositories/snapshots -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn assembly:single -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn deploy:deploy-file -Dcuba.repository.url=%UPLOAD_REPO_PATH% -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
 
 cd ../..
 
 cd modules\activiti-json-converter
-rem call mvn package -Dcuba.modeler.version=%MODELER_VERSION% -P cuba,distro
-call mvn deploy:deploy-file -Dcuba.repository.url=http://10.1.20.55:8082/nexus/content/groups/cuba-group/repositories/snapshots -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn package -Dcuba.modeler.version=%MODELER_VERSION% -P cuba,distro
+call mvn deploy:deploy-file -Dcuba.repository.url=%UPLOAD_REPO_PATH% -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
 
 cd ../..
 
--- upload-archives.bat
+++ upload-archives.bat
@@ -1,16 +1,14 @@
-set MODELER_VERSION=5.20.bespoke.10-SNAPSHOT
+set MODELER_VERSION=5.20.bespoke.11
+set UPLOAD_REPO_PATH=http://repository.haulmont.com:8587/nexus/content/repositories/releases
 
 cd modules\activiti-webapp-explorer2
-rem call mvn assembly:single -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
-call mvn deploy:deploy-file -Dcuba.repository.url=http://repository.haulmont.com:8587/nexus/content/repositories/snapshots -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn assembly:single -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn deploy:deploy-file -Dcuba.repository.url=%UPLOAD_REPO_PATH% -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
 
 cd ../..
 
 cd modules\activiti-json-converter
-rem call mvn package -Dcuba.modeler.version=%MODELER_VERSION% -P cuba,distro
-call mvn deploy:deploy-file -Dcuba.repository.url=http://repository.haulmont.com:8587/nexus/content/repositories/snapshots -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
+call mvn package -Dcuba.modeler.version=%MODELER_VERSION% -P cuba,distro
+call mvn deploy:deploy-file -Dcuba.repository.url=%UPLOAD_REPO_PATH% -Dcuba.modeler.version=%MODELER_VERSION% -P cuba --settings settings.xml
 
 cd ../..
-
-
-
